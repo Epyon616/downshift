ea2b90b8d0603559aae3cefdfc5a413cba5bc8c0
Lee Richmond <lee@downshift.io>
2024-09-27 09:51:20 +0100

    update project with maintenance page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Email } from 'react-obfuscate-email';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header hazard-background'>&nbsp;</div>
+      <div className="content">
+        <div className="logo">
+          <span data-text="DOWN"></span>
+          <span data-text="SHIFT"></span>
+        </div>
+        <div className="code-text">
+          <h2>The home of full stack developer Lee Richmond</h2>
+          <p>My website is currently undergoing some exciting changes, but don't worry - it will be back online very soon!</p> 
+          <p>Thanks for your patience and stay tuned for updates!</p>
+          <p>In the meantime if you would like to discuss a project with me drop me an <Email email="lee@downshift.io" subject="Enquiery from your portfolio site">email</Email></p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
