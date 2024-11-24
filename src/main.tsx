6daecbc31d203e6a6ed5c54fe8564153694f98cb
Lee Richmond <lee@downshift.io>
2024-11-24 23:35:09 +0000

    styling update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ConfigsProvider } from './Components/Contexts';
 import configData from './conf/config.json';
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
