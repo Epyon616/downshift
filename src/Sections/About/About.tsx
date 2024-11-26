2bcdf5e9cace24cc5c4b734239d250d506d887cf
Lee Richmond <lee@downshift.io>
2024-11-26 21:41:43 +0000

    update about

--- src/Sections/About/About.tsx
+++ src/Sections/About/About.tsx
@@ -8,8 +8,8 @@ const AboutSection = () => {
     <div className="about-content">
       <h4 className="subtitle">The home of full stack developer <em>{configs.name}</em></h4>
       <p>My website is currently undergoing some exciting changes, but don't worry - it will be back online very soon!</p> 
-      <p>Thanks for your patience and stay tuned for updates!</p>
       <p>In the meantime if you would like to discuss a project with me drop me a message using the form below.</p>
+      <p>Thanks for your patience and stay tuned for updates!</p>
     </div>
   );
 }
