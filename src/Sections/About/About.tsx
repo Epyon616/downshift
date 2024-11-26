0e5ce9c799f97e92c93952e3062a83de505ef3c5
Lee Richmond <lee@downshift.io>
2024-11-26 21:38:55 +0000

    update about section

--- src/Sections/About/About.tsx
+++ src/Sections/About/About.tsx
@@ -6,7 +6,7 @@ const AboutSection = () => {
   const { configs } = useContext(ConfigContext);
   return (
     <div className="about-content">
-      <h2>The home of full stack developer <em>{configs.name}</em></h2>
+      <h4 className="subtitle">The home of full stack developer <em>{configs.name}</em></h4>
       <p>My website is currently undergoing some exciting changes, but don't worry - it will be back online very soon!</p> 
       <p>Thanks for your patience and stay tuned for updates!</p>
       <p>In the meantime if you would like to discuss a project with me drop me a message using the form below.</p>
