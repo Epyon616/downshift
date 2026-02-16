f188f8ca2ecdf5525c7e48ad7a34cf1c41b47050
Lee Richmond <lee@downshift.io>
2026-02-16 18:06:47 +0000

    fix

--- src/Sections/About/About.tsx
+++ src/Sections/About/About.tsx
@@ -1,20 +1,17 @@
 import { useContext } from 'react';
 import { ConfigContext } from '../../Components/Contexts';
 import { IconLink } from '../../Components/Links';
-import ContentBlock from '../../Components/ContentBlock';
 import GithubIcon from '../../assets/github.svg';
 import LinkedInIcon from '../../assets/linkedin.svg';
 import './About.scss';
 
 const AboutSection = () => {
   const { configs } = useContext(ConfigContext);
-  const  { name, aboutSection, links } = configs;
+  const  { links } = configs;
   const { github, linkedin } = links;
 
   return (
     <div className="about-content">
-      <h4 className="subtitle">{aboutSection.title} <em>{name}</em></h4>
-      <ContentBlock content={aboutSection.description} />
       <div>
         <IconLink href={github.link} label={github.label} icon={GithubIcon} />
         <IconLink href={linkedin.link} label={linkedin.label} icon={LinkedInIcon} />
