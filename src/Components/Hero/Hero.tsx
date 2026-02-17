876b42f451f1b9d02892b0493a360476a98dd1ae
Lee Richmond <lee@downshift.io>
2026-02-17 09:17:41 +0000

    update hero scroll cta

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ const HeroImage = () => {
         <div className="hero-text">
           <span className="logo"></span><strong>down</strong>shift
         </div>
-        <span className="scroll blink-smooth">Scroll</span>
+        <span className="scroll blink-smooth">Scroll <br /> &#x25BC;</span>
       </div>
     </div>
   );
