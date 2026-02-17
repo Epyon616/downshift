482d62546712d51542ae78e0c3e67617cc08558b
Lee Richmond <lee@downshift.io>
2026-02-17 09:29:18 +0000

    make logo fully responsive

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,11 +1,12 @@
 import './Hero.scss'
+import downshiftLogo from '../../assets/ds-chevron.svg';
 
 const HeroImage = () => {
   return (
     <div className="hero-image">
       <div className="overlay">
         <div className="hero-text">
-          <span className="logo"></span><strong>down</strong>shift
+          <img src={downshiftLogo} className="logo" /><strong>down</strong>shift
         </div>
         <span className="scroll blink-smooth">Scroll <br /> &#x25BC;</span>
       </div>
